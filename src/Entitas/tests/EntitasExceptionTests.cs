ebef0df78df3917ac1f5b659fbc21d530231b5cb
Simon Schmid <mail@sschmid.com>
2021-09-30 08:27:06 +0200

    Add EntitasExceptionTests

---
+++ src/Entitas/tests/EntitasExceptionTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Entitas.Tests
+{
+    public class EntitasExceptionTests
+    {
+        [Fact]
+        public void CreatesExceptionWithHintSeparatedByNewLine()
+        {
+            const string msg = "Message";
+            const string hint = "Hint";
+            var exception = new EntitasException(msg, hint);
+            exception.Message.Should().Be(msg + "\n" + hint);
+        }
+
+        [Fact]
+        public void IgnoresHintWhenNull()
+        {
+            const string msg = "Message";
+            const string hint = null;
+            var ex = new EntitasException(msg, hint);
+            ex.Message.Should().Be(msg);
+        }
+    }
+}
